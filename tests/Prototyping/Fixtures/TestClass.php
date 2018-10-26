5b38bcee58202f25c400871c5ae97d95ab24bbb8
Wolfy-J <wolfy.jd@gmail.com>
2018-10-26 15:51:59 -0700

    annotation modifications

--- tests/Prototyping/Fixtures/TestClass.php
+++ tests/Prototyping/Fixtures/TestClass.php
@@ -8,24 +8,11 @@
 
 namespace Spiral\Prototyping\Tests\Fixtures;
 
-use Spiral\Core\Exception\InvalidArgumentException;
 use Spiral\Prototyping\Traits\PrototypeTrait;
-use Spiral\Prototyping\Tests\Fixtures\HydratedClass as X;
 
 class TestClass
 {
     use PrototypeTrait;
-
-    /**
-     * TestClass constructor.
-     *
-     * @param X $h
-     *
-     * @throws InvalidArgumentException
-     */
-    public function __construct(X $h)
-    {
-    }
 
     public function getSelf(): self
     {
